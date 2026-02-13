fbbe5fb225cab2a723104b165e1115005b03f8d6
Gael <gaelcarrillo995@gmail.com>
2026-02-12 20:07:39 -0800

    finished it with the go fund me account

--- pages/galleryMain.php
+++ pages/galleryMain.php
@@ -1,10 +1,11 @@
-<?php
+ <?php
 
-echo <div class="container">
-    echo <div class="photo">
-            <img src="" alt="">
-    echo </div>
-echo </div>
+// echo <div class="container">
+//     echo <div class="photo">
+//             <img src="" alt="">
+//     echo </div>
+// echo </div>
+//Not going to be used
     
 
 
